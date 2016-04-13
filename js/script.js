2dd9b08cb3f774ef21da484b588d70c0780f8d99
Salvo <salvo.guastella90@gmail.com>
2016-04-13 08:42:24 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+var _post=function(){
+	item_id:"";
+	item_name:"";
+	service_name:"";
+	item_published:"";
+	getElem:"";
+}
+
+var posts=[];
+
+$(document).ready(function() {
+
+
+	$.ajax({
+		url: './data/posts.json',
+		type: 'POST'
+	})
+	.done(function(data) {
+		console.log(data['items']);
+		  $.each( data['items'], function( key, val ) {
+		    var myPost=new _post();
+		    var _src=data['items'][key];
+		    myPost.item_id=_src['item_id'];
+		    myPost.item_name=_src['item_name'];
+		    myPost.service_name=_src['service_name'];
+		    myPost.item_published=_src['item_published'];
+		    var _data=_src['item_data'];
+		    switch(_src['service_name']){
+		    	case 'Manual':
+		    		myPost.image_id=_data['image_id'];
+		    		myPost.image_url=_data['image_url'];
+		    		myPost.text=_data['text'];
+		    		myPost.link=_data['link'];
+		    		myPost.link_text=_data['link_text'];
+
+		    		myPost.getElem=function(){
+		    			var elem = "";
+		    			elem+="<div class='"+this.service_name+"' rel='"+this.item_id+"'>";
+			    			elem+="<img src='"+this.image_url+"' rel='"+this.image_id+"' />";
+			    			elem+="<p>"+this.text+"</p>";
+			    			elem+="<a href='"+this.link+"'' target='_blank'>"+this.link_text+"</a>";
+		    			elem+="</div>";
+
+		    			return elem;
+		    		};
+		    		break;
+
+		    	case 'Twitter':
+		    		myPost.tweet=_data['tweet'];
+		    		var _user=_data['user'];
+		    		var _account=_src['account_data'];
+		    		myPost.username=_user['username'];
+		    		myPost.avatar=_user['avatar'];
+		    		myPost.user_id=_account['user_id'];
+
+		    		myPost.getElem=function(){
+		    			var elem = "";
+		    			elem+="<div class='"+this.service_name+"' rel='"+this.item_id+"'>";
+			    			elem+="<img src='"+this.avatar+"' rel='"+this.user_id+"' />";
+			    			elem+="<span>"+this.username+"</span>";
+			    			elem+="<p>"+findTwitterUsername(findTwitterHashtag(findUrl(this.tweet)))+"</p>";
+		    			elem+="</div>";
+
+
+		    			return elem;
+		    		};
+		    		break;
+
+		    	case 'Instagram':
+		    		myPost.link=_data['link'];
+		    		myPost.caption=_data['caption'];
+		    		var _user=_data['user'];
+		    		var _account=_src['account_data'];
+		    		var _image=_data['image'];
+		    		myPost.username=_user['username'];
+		    		myPost.avatar=_user['avatar'];
+		    		myPost.user_id=_account['user_id'];
+		    		myPost.thumb=_image['thumb'];
+		    		myPost.image=_image['large'];
+
+		    		myPost.getElem=function(){
+		    			var elem = "";
+		    			elem+="<div class='"+this.service_name+"' rel='"+this.item_id+"'>";
+			    			elem+="<a href='"+this.link+"' target='_blank'><img src='"+this.thumb+"' rel='"+this.user_id+"' /></a>";
+			    			elem+="<span>"+findInstagramHashtag(findUrl(this.caption));+"</span1>";
+		    			elem+="</div>";
+
+		    			return elem;
+		    		};
+		    		break;
+
+		    	default:
+		    		console.log("Error");
+		    }
+		    posts.push(myPost);
+		    $(".main ul").append("<li>"+myPost.getElem()+"</li>");
+		  });
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
+});
+
+function findUrl(string){
+	return string.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
+		return url.link(url);
+	});
+};
+
+function findTwitterUsername(string){
+	return string.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+		var username = u.replace("@","")
+		return u.link("http://twitter.com/"+username);
+	});
+};
+
+function findTwitterHashtag(string){
+	return string.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+		var tag = t.replace("#","%23")
+		return t.link("http://search.twitter.com/search?q="+tag);
+	});
+};
+
+function findInstagramHashtag(string){
+	return string.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+		var tag = t.replace("#","%23")
+		return t.link("https://www.instagram.com/explore/tags/"+tag);
+	});
+};
